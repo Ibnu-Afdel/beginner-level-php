59692cbc5038317d2c18332ecb8edb525c021ca8
Ibnu Afdel <ibnuafdel@gmail.com>
2025-01-18 23:31:24 +0300

    Add styles

--- QuranVerses/index.php
+++ QuranVerses/index.php
@@ -65,6 +65,7 @@ if ($responseSahih !== false && $responseAmharic !== false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Random Quranic Verse</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
