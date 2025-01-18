01861fd2bd52101377fa2bf655300da96b939f88
Ibnu Afdel <ibnuafdel@gmail.com>
2025-01-18 23:16:00 +0300

    Add Random Butoon to the qoute generator one button

--- QuranVerses/index.php
+++ QuranVerses/index.php
@@ -6,22 +6,24 @@
     <title>Random Quote Generator</title>
 </head>
 <body>
+    <?php
+    $quotes = [
+        ["quote" => "The only way to do great work is to love what you do.", "author" => "Steve Jobs"],
+        ["quote" => "Success is not the key to happiness. Happiness is the key to success.", "author" => "Albert Schweitzer"],
+        ["quote" => "Don't watch the clock; do what it does. Keep going.", "author" => "Sam Levenson"],
+        ["quote" => "Keep your face always toward the sunshine—and shadows will fall behind you.", "author" => "Walt Whitman"],
+        ["quote" => "Believe you can and you're halfway there.", "author" => "Theodore Roosevelt"]
+    ];
+
+    $randomIndex = array_rand($quotes);
+    $randomQuote = $quotes[$randomIndex];
+    ?>
     <div>
-        <?php
-        $quotes = [
-            ["quote" => "The only way to do great work is to love what you do.", "author" => "Steve Jobs"],
-            ["quote" => "Success is not the key to happiness. Happiness is the key to success.", "author" => "Albert Schweitzer"],
-            ["quote" => "Don't watch the clock; do what it does. Keep going.", "author" => "Sam Levenson"],
-            ["quote" => "Keep your face always toward the sunshine—and shadows will fall behind you.", "author" => "Walt Whitman"],
-            ["quote" => "Believe you can and you're halfway there.", "author" => "Theodore Roosevelt"]
-        ];
-
-        $randomIndex = array_rand($quotes);
-        $randomQuote = $quotes[$randomIndex];
-
-        echo "<div>\"{$randomQuote['quote']}\"</div>";
-        echo "<div'>- {$randomQuote['author']}</div>";
-        ?>
+        <div>"<?php echo $randomQuote['quote']; ?>"</div>
+        <div>- <?php echo $randomQuote['author']; ?></div>
     </div>
+    <form method="post">
+        <button type="submit">Get Random Quote</button>
+    </form>
 </body>
 </html>
